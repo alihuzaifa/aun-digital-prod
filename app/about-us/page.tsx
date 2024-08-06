31b952967809f98b6c7b9406b0c894ce1e36125b
alihuzaifa <ha4587323@gmail.com>
2024-08-06 14:21:58 +0500

    new build

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -11,6 +11,7 @@ import Contact from "../components/about/contact";
 import type { Metadata } from "next";
 import { metadataJson } from "@/metadata";
 
+
 export async function generateMetadata(): Promise<Metadata> {
   // Fetch metadata from the JSON file
   const pageMetadata = metadataJson["about"];
