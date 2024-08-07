207454d68edfdb2ead4160e2551a5c1791b8794e
alihuzaifa <ha4587323@gmail.com>
2024-08-07 07:58:30 +0500

    correct the gtm issue in my code

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,8 @@ import "./globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import TalkTo from "./components/talkTo";
-import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
-
+import { GoogleTagManager } from '@next/third-parties/google'
 export const metadata: Metadata = {
   title: "Aun Digital",
   description: "Aun Digital description",
@@ -20,33 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <GoogleTagManager gtmId="GTM-NC2Z4F2D" />
         <Header />
-        <Script
-          id="google-tag-manager"
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/ns.html?id=GTM-NC2Z4F2D"
-        />
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(w,d,s,l,i) {
-              w[l]=w[l]||[];
-              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NC2Z4F2D');
-          `,
-          }}
-        />
         {children}
         <TalkTo />
         <Footer />
-        {/* </CursorProvider> */}
       </body>
     </html>
   );
