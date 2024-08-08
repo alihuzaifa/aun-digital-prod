6c67f9c2547d63fd80c6f6527c185d631dc7cac2
alihuzaifa <ha4587323@gmail.com>
2024-08-08 21:04:05 +0500

    videoo is also visible on mobile screen

--- app/components/videoBackground.tsx
+++ app/components/videoBackground.tsx
@@ -2,23 +2,7 @@ import React from "react";
 
 const VideoBackground: React.FC = () => {
   return (
-    // <div
-    //   className="relative w-full hidden md:block"
-    //   style={{ paddingBottom: "56.25%" }}
-    // >
-    //   <iframe
-    //     src="https://player.vimeo.com/video/995418774?h=58956de3da&background=1"
-    //     title="Video"
-    //     className="absolute top-0 left-0 w-full h-full"
-    //     style={{ border: "none" }}
-    //     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    //     allowFullScreen
-    //   ></iframe>
-    // </div>
-    <div
-      className="relative w-full hidden md:block"
-      style={{ paddingBottom: "56.25%" }}
-    >
+    <div className="relative w-full " style={{ paddingBottom: "56.25%" }}>
       <video
         src="/bg.mp4"
         title="Background Video"
