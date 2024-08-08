60d05f68e1b70c8901422e7f4fb98f41cc082e65
alihuzaifa <ha4587323@gmail.com>
2024-08-08 13:09:30 +0500

    now also place a video with video tag just for chceking performance

--- app/components/videoBackground.tsx
+++ app/components/videoBackground.tsx
@@ -2,18 +2,33 @@ import React from "react";
 
 const VideoBackground: React.FC = () => {
   return (
+    // <div
+    //   className="relative w-full hidden md:block"
+    //   style={{ paddingBottom: "56.25%" }}
+    // >
+    //   <iframe
+    //     src="https://player.vimeo.com/video/995418774?h=58956de3da&background=1"
+    //     title="Video"
+    //     className="absolute top-0 left-0 w-full h-full"
+    //     style={{ border: "none" }}
+    //     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    //     allowFullScreen
+    //   ></iframe>
+    // </div>
     <div
       className="relative w-full hidden md:block"
       style={{ paddingBottom: "56.25%" }}
     >
-      <iframe
-        src="https://player.vimeo.com/video/995418774?h=58956de3da&background=1"
-        title="Video"
-        className="absolute top-0 left-0 w-full h-full"
+      <video
+        src="/bg.mp4"
+        title="Background Video"
+        className="absolute top-0 left-0 w-full h-full object-cover"
         style={{ border: "none" }}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+        autoPlay
+        loop
+        muted
+        playsInline
+      />
     </div>
   );
 };
